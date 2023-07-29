a92bef09c7b33679fbb881a9f184dccdd81049d9
Fernando Nicolas Zelarayan <98678489+NicolasZelarayan@users.noreply.github.com>
2023-07-29 01:19:48 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,18 @@
-import psycopg2 as bd
+import psycopg2
+import time
 
-conexion = bd.connect(
-    user= 'chiperos',
-    password = 'vacunarg123.',
-    host='db',
-    port='5432',
-    database = 'vacunarg'
-)
+while True:
+    try:
+        conexion = psycopg2.connect(
+            user='chiperos',
+            password='vacunarg123.',
+            host='db',
+            port='5432',
+            database='vacunatorio'
+        )
+        # Si llegamos a este punto, la conexión fue exitosa, por lo que salimos del ciclo
+        break
+    except psycopg2.OperationalError as e:
+        print('Waiting for connecting to PostgreSQL...')
+        # Esperamos un poco antes de volver a intentarlo
+        time.sleep(1)
