588f0db21ac71e4f23bcbd8a58a52ed66e0564f6
Fernando Nicolas Zelarayan <98678489+NicolasZelarayan@users.noreply.github.com>
2023-07-29 00:40:54 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import psycopg2 as bd
 conexion = bd.connect(
     user= 'chiperos',
     password = 'vacunarg123.',
-    host='localhost',
+    host='172.18.0.2',
     port='5432',
     database = 'vacunatorio'
 )
